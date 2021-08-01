c837ec14885abadf0dd466f873ba88308b726cdc
Deann Huang <deann.huang@linecorp.com>
2021-08-02 03:57:30 +0800

    remove the check box

--- src/components/customers/List.tsx
+++ src/components/customers/List.tsx
@@ -3,7 +3,7 @@ import { Datagrid, DateField, List, TextField } from "react-admin";
 import CustomerLinkField from '@/components/customers/CustomerLinkField';
 
 const CustomerList = props => (
-    <List {...props} pagination={false}>
+    <List {...props} pagination={false} bulkActionButtons={false}>
         <Datagrid rowClick="show" >
             <CustomerLinkField />
             <TextField source="gatherCode" />
