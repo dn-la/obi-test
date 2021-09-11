ce1bb1e27c9aadbbb47998812fbbf686bdf81bd5
Deann Huang <deann.huang@linecorp.com>
2021-09-11 13:19:57 +0800

    step 3: add Cutomerlist

--- src/components/ReactAdmin.tsx
+++ src/components/ReactAdmin.tsx
@@ -1,14 +1,14 @@
 import { FunctionComponent } from 'react';
-import { Admin, ListGuesser, Resource } from 'react-admin';
+import { Admin, Resource} from 'react-admin';
 import dataProvider from '@/dataProvider';
-// import CustomerList  from '@/components/customers/List';
+import CustomerList  from '@/components/customers/List';
 import Layout from '@/layout/Layout';
 
 const ReactAdmin: FunctionComponent = () => {
 
   return (
     <Admin dataProvider={dataProvider} layout={Layout}>
-      <Resource name="customers"/>
+      <Resource name="customers" list={CustomerList} />
     </Admin>
   );
 };
--- src/components/customers/List.tsx
+++ src/components/customers/List.tsx
@@ -4,7 +4,7 @@ import CustomerLinkField from '@/components/customers/CustomerLinkField';
 
 const CustomerList = props => (
     <List {...props} pagination={false} bulkActionButtons={false}>
-        <Datagrid rowClick="show" >
+        <Datagrid rowClick="edit" >
             <CustomerLinkField />
             <TextField source="gatherCode" />
             <DateField source="createdAt" />
